03e4b64a86b2e70adbaddedf633f114a219c2492
jasonkaiser <alexander.jason.kaiser@stu.ibu.edu.ba>
2024-10-09 11:16:39 +0200

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,28 @@
 package org.example;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        // Task 1
+
+        Scanner scanner = new Scanner(System.in);
+        String password_key = "secret";
+
+        while(true){
+
+           System.out.println("Enter your password: ");
+           String password_input = scanner.nextLine();
+
+           if(password_input.equals(password_key)){
+               System.out.println("Right, the password is: " + password_input);
+               break;
+           }
+
         }
+
     }
 }
